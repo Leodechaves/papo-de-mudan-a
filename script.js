075e2b50dd6f884f6dddc85ba4157a6fabfd6047
Leodechaves <166830229+Leodechaves@users.noreply.github.com>
2024-05-03 10:36:29 -0300

    222

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const bloco = document.getElementById('bloco');
-    let posicaoX = parseFloat(window.getComputedStyle(bloco).left);
-    let posicaoY = parseFloat(window.getComputedStyle(bloco).top);
-    const velocidade = 10; // Ajuste conforme necessário
+    const jogador = document.getElementById('jogador');
+    const labirinto = document.getElementById('labirinto');
+
+    const larguraJogador = parseFloat(window.getComputedStyle(jogador).width);
+    const larguraLabirinto = parseFloat(window.getComputedStyle(labirinto).width);
+    const velocidade = 10;
 
     document.addEventListener('keydown', function(event) {
+        let posicaoX = parseFloat(window.getComputedStyle(jogador).left);
+        let posicaoY = parseFloat(window.getComputedStyle(jogador).top);
+
         switch(event.key) {
             case 'ArrowLeft':
                 posicaoX -= velocidade;
+                if (posicaoX < 0) posicaoX = 0;
                 break;
             case 'ArrowRight':
                 posicaoX += velocidade;
+                if (posicaoX > larguraLabirinto - larguraJogador) posicaoX = larguraLabirinto - larguraJogador;
                 break;
             case 'ArrowUp':
                 posicaoY -= velocidade;
+                if (posicaoY < 0) posicaoY = 0;
                 break;
             case 'ArrowDown':
                 posicaoY += velocidade;
+                if (posicaoY > larguraLabirinto - larguraJogador) posicaoY = larguraLabirinto - larguraJogador;
                 break;
         }
-        bloco.style.left = `${posicaoX}px`;
-        bloco.style.top = `${posicaoY}px`;
+
+        jogador.style.left = `${posicaoX}px`;
+        jogador.style.top = `${posicaoY}px`;
     });
 });
